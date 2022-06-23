a0cd4ece66396b0543dbbb8029026ee255478088
onur <onur@pow.games>
2022-06-23 13:02:08 +0300

    fix: Naming format

--- SO/AdjustEventHandler.cs
+++ SO/AdjustEventHandler.cs
@@ -6,17 +6,17 @@ namespace pow.hermes
     [CreateAssetMenu(fileName = "AdjustEventHandler", menuName = "POW_SDK/Hermes/AdjustEventHandler", order = 0)]
     public class AdjustEventHandler : ScriptableObject
     {
-        private List<PowAdjustEvent> adjustEvents = new List<PowAdjustEvent>();
+        private List<PowAdjustEvent> _adjustEvents = new List<PowAdjustEvent>();
 
-        public void AddPowAdjustEvent(PowAdjustEvent powAdjustEvent)
+        public void AddPowAdjustEvent(string key, string token)
         {
-            adjustEvents.Add(powAdjustEvent);
+            _adjustEvents.Add(new PowAdjustEvent(key, token));
         }
 
-        public string GetAjustEventTokenByKey(string key)
+        public string GetAdjustEventTokenByKey(string key)
         {
-            PowAdjustEvent powAdjustEvent = adjustEvents.Find(e => e._key == key);
-            return powAdjustEvent == null ? string.Empty : adjustEvents.Find(e => e._key == key)._token;
+            PowAdjustEvent powAdjustEvent = _adjustEvents.Find(e => e._key == key);
+            return powAdjustEvent == null ? string.Empty : _adjustEvents.Find(e => e._key == key)._token;
         }
     }
 }
