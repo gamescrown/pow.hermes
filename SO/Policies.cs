1481b561e58a77a0a036ea03c2cdbc577c14df8a
onur <onur@pow.games>
2022-07-27 18:41:22 +0300

    feat: Add policies reset function

--- SO/Policies.cs
+++ SO/Policies.cs
@@ -48,5 +48,11 @@ namespace pow.hermes
             isAccepted = reader.ReadBoolean();
             hasUserConsent = reader.ReadInt32();
         }
+
+        public override void Reset()
+        {
+            base.Reset();
+            isAccepted = false;
+        }
     }
 }
